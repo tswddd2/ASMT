f90b410844103f5d99b662059b44faa6b5b9f2dc
tswddd <tswddd@gmail.com>
2025-09-26 10:04:38 +0900

    adjust atabuilder

--- ASMT_check.cpp
+++ ASMT_check.cpp
@@ -4,16 +4,12 @@
 #include "BM_old.cpp"
 using namespace std;
 
-const int N = 1000000, Trials = 10;
+const int Trials = 10;
 
 int main() {
     for (int trial = 0; trial < Trials; trial++) {
 
         cout << "Trial " << trial + 1 << ":\n";
-
-        int n1 = N, n2 = N;
-        int sqrtn = (int)sqrt(n2);
-        int m = 0;
 
         // databuilder
         system("databuilder.exe");
@@ -31,13 +27,6 @@ int main() {
         clock_t end2 = clock();
         double elapsed2 = double(end2 - start2) / CLOCKS_PER_SEC;
         cout << elapsed2 << " seconds\n";
-
-        // if (mis1 != mis2) {
-        //     cout << "Mismatch!\n";
-        //     return -1;
-        // } else {
-        //     cout << "Match.\n";
-        // }
     }
 
     return 0;
--- databuilder.cpp
+++ databuilder.cpp
@@ -5,19 +5,15 @@
 #include <string>
 using namespace std;
 
-int N = 100;
+int N = 10;
 
-// Binary tree node structure
+// Generalized tree node structure
 struct TreeNode {
     int label; // label of leaf node, -1 for internal nodes
-    TreeNode* left;
-    TreeNode* right;
-    TreeNode(int l) : label(l), left(nullptr), right(nullptr) {}
-    TreeNode(TreeNode* l, TreeNode* r) : label(-1), left(l), right(r) {}
-    ~TreeNode() {
-        delete left;
-        delete right;
-    }
+    vector<TreeNode*> children;
+
+    TreeNode(int l) : label(l) {}
+    TreeNode(const vector<TreeNode*>& ch) : label(-1), children(ch) {}
 };
 
 mt19937 rng(static_cast<unsigned int>(time(nullptr)));
@@ -25,12 +21,11 @@ mt19937 rng(static_cast<unsigned int>(time(nullptr)));
 // Generate a random binary tree
 TreeNode* randomBinaryTree(int N) {
     if (N <= 0) return nullptr;
-    // Initialize N leaf nodes
     vector<TreeNode*> nodes;
     for (int i = 0; i < N; ++i) {
         nodes.push_back(new TreeNode(i));
     }
-    // Random number generator
+
     for (int i = 0; i < N - 1; ++i) {
         // Randomly select two different nodes to merge
         uniform_int_distribution<int> dist(0, nodes.size() - 1);
@@ -38,13 +33,10 @@ TreeNode* randomBinaryTree(int N) {
         int idx2 = dist(rng);
         while (idx2 == idx1) idx2 = dist(rng);
 
-        TreeNode* left = nodes[idx1];
-        TreeNode* right = nodes[idx2];
+        vector<TreeNode*> ch = { nodes[idx1], nodes[idx2] };
+        TreeNode* newNode = new TreeNode(ch);
 
-        // Create new node
-        TreeNode* newNode = new TreeNode(left, right);
-
-        // Add new node to the list and remove the merged nodes
+        // Remove merged nodes and add the new node
         if (idx1 > idx2) swap(idx1, idx2);
         nodes.erase(nodes.begin() + idx2);
         nodes.erase(nodes.begin() + idx1);
@@ -53,15 +45,19 @@ TreeNode* randomBinaryTree(int N) {
     return nodes[0];
 }
 
-// Generate string representation in the specified format
+// Generate string representation (children separated by "|")
 string treeToString(TreeNode* root) {
     if (!root) return "";
     if (root->label != -1) {
         return to_string(root->label);
     } else {
-        string leftStr = treeToString(root->left);
-        string rightStr = treeToString(root->right);
-        return "(" + leftStr + "|" + rightStr + ")";
+        string res = "(";
+        for (size_t i = 0; i < root->children.size(); ++i) {
+            if (i) res += "|";
+            res += treeToString(root->children[i]);
+        }
+        res += ")";
+        return res;
     }
 }
 
